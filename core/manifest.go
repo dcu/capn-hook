89442f0496c4942ee6318e9bb4762c4e9dc14cda
David A. Cuadrado <krawek@gmail.com>
2016-02-27 15:52:30 -0500

    Add command to generate the default config.
    For now it supports both ruby and go.

--- core/manifest.go
+++ core/manifest.go
@@ -60,7 +60,7 @@ func FindManifest() (*Manifest, error) {
 	return findManifestIn(wd, 0)
 }
 
-// DefaultGolangManifest returns a default maniest for golang
+// DefaultGolangManifest returns a default manifest for golang
 func DefaultGolangManifest() *Manifest {
 	return &Manifest{
 		PrepareCommitMsg: []*Hook{
@@ -87,6 +87,49 @@ func DefaultGolangManifest() *Manifest {
 				Pattern: "glide.*",
 				Run: []string{
 					"glide install",
+				},
+			},
+		},
+	}
+}
+
+// DefaultRubyManifest returns a default manifest for ruby
+func DefaultRubyManifest() *Manifest {
+	return &Manifest{
+		PrepareCommitMsg: []*Hook{
+			&Hook{
+				Pattern: "*.rb",
+				Run: []string{
+					"rubycritic -f console {files}",
+				},
+			},
+			&Hook{
+				Pattern: "Gemfile*",
+				Run: []string{
+					"dawn -z -K .",
+				},
+			},
+		},
+		PostReceive: []*Hook{
+			&Hook{
+				Pattern: "Gemfile*",
+				Run: []string{
+					"bundle install",
+				},
+			},
+		},
+	}
+}
+
+// DefaultManifest returns a default manifest
+func DefaultManifest() *Manifest {
+	return &Manifest{
+		PreCommit: []*Hook{
+			&Hook{
+				Pattern: "*",
+				Run: []string{
+					"echo {files}",
+					"echo {file}",
 				},
 			},
 		},
